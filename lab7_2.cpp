d20a7d2fd56d208256f27c4ebe6dba61f082f3e3
Phanthat Muenprap <phanthat54@gmail.com>
2021-11-30 15:46:12 +0700

    lab7_2 Finish

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,11 +3,34 @@
 using namespace std;
 
 int main(){
-	
-	int i = 0, P[5000], k, result;
+	// Variable declaration
+	int i, k, P[5000], result;
+
+	// Make Array P = (1-9999)
+	i = 0;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
+	}
+	
+	// Input k
+	cout << "Please input k: ";
+	cin >> k;
+
+	//  Main program
+	if (k > 0){
+		i = 0, result = 0;
+		while (i < 5000){
+			if (P[i] % k == 0){
+				result -= P[i];
+			} else{
+				result += P[i];
+			}
+			i++;
+		}
+			cout <<"Result = " <<result <<endl;
+	} else{
+		cout << "Invalid input!!!" <<endl;
 	}
 	
 	return 0;

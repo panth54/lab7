77573107454582ce041ace714df983876f49ff08
Phanthat Muenprap <phanthat54@gmail.com>
2021-11-30 15:13:51 +0700

    lab7_1 Finish

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Func reversed text
 string func1(string x){
 	int i = 0, L = x.size();
 	string y = "";
@@ -13,6 +14,7 @@ string func1(string x){
 	return y;
 }
 
+// Func to upper case
 string func2(string x){
 	int i = 0, L = x.size();
 	string y = "";
@@ -23,6 +25,7 @@ string func2(string x){
 	return y;	
 }
 
+//Func to lower case
 string func3(string x){
 	int i = 0, L = x.size();
 	string y = "";
@@ -34,8 +37,33 @@ string func3(string x){
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+	// variable declaration
+	string text = "";
+	string reversedText = "";
+	string upperText = "";
+	string upperReversedText = "";
+	string result = "";
+
+	// input text
+    cout << "Input text: ";
+	cin >> text;
+	
+	// display reversed text
+	reversedText = func1(text);
+    cout << "Reversed text: " <<reversedText <<endl;
+
+	// change to upper case
+	upperText = func3(text);
+	upperReversedText = func3(reversedText);
+
+	// Palindrome check
+	if (upperText == upperReversedText){
+		result = "Yes";
+	} else{
+		result = "No";
+	}
+	// display result
+    cout << "Palindrome: " <<result <<endl;
+	
     return 0;
 }

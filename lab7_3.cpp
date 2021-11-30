5a4f71d0bd07e4b487e2f0356c0d3ad138b8d418
Phanthat Muenprap <phanthat54@gmail.com>
2021-11-30 16:08:44 +0700

    lab7_3 Finish

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,26 @@
-#include<iostream>
-
+#include <iostream>
+#include <cmath>
 using namespace std;
 
+int adiff(int a,int b){
+  a %= 360;
+  b %= 360;
+  int result = abs(a-b);
+  if (result > 180){
+    result = 360 - result;
+  }
+  return result;
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) <<endl;
+  cout << adiff(210,45) <<endl;
+  cout << adiff(0,360) <<endl;
+  cout << adiff(10,350) <<endl;
+  cout << adiff(95,260) <<endl;
+  cout << adiff(90,-90) <<endl;
+  cout << adiff(1000,280) <<endl;
+  cout << adiff(60,244) <<endl;
+  
+  return 0;
 }
